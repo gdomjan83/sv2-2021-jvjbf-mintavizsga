14644827c4df64637d4b9a3e755084e47ebcd1d0
J_C <gomjan83@gmail.com>
2021-12-12 12:00:59 +0100

    Fish excercise updated.

--- src/main/java/aquarium/Tang.java
+++ src/main/java/aquarium/Tang.java
@@ -1,7 +1,6 @@
 package aquarium;
 
 public class Tang extends Fish {
-    private boolean memoryLoss;
 
     public Tang(String name, int weight, String color) {
         super(name, weight, color);
